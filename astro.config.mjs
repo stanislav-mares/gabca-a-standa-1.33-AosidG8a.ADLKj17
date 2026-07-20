87a0c799c2bba004d176ced3413d43e87c1acdb2
Stanislav Mareš <stanislav.mares92@gmail.com>
2026-07-20 11:32:53 +0200

    change base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stanislav-mares.github.io',
-  base: '/gabca-a-standa-svatba/',
+  base: '/gabca-a-standa-1.33-AosidG8a.ADLKj17/',
   vite: {
     plugins: [tailwindcss()]
   }
